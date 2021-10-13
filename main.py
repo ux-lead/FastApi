5c8c0547827ac67aa75ca330f0dd6d4c83f6c8d6
ux-lead <renato.silvestre1@hotmail.com>
2021-10-13 16:48:10 -0300

    redução do código

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ app = FastAPI()
 
 
 @app.get("/")
-async def speed():
+async def speed_0():
     return {"message": "Hello World"}
 
 @app.get("/home")
-async def speed():
+async def speed_1():
     return {"user": {
         "nome": "Renato",
         "CPF": ""
